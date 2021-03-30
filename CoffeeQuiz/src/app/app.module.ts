423dae63a9bed197f537616fa969848fc9a1dc66
BigG <gabszon002@gmail.com>
2021-03-30 16:39:00 +0200

    Imported Forms

--- CoffeeQuiz/src/app/app.module.ts
+++ CoffeeQuiz/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, QuizComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
