96fdd922adb123415aebeab711f18fb6b4fb36cb
BigG <gabszon002@gmail.com>
2021-03-30 18:36:00 +0200

    Imported HttpClientModule

--- CoffeeQuiz/src/app/app.module.ts
+++ CoffeeQuiz/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
 
 @NgModule({
   declarations: [AppComponent, QuizComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
