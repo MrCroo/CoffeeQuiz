53b44bf3a6cfb1d94d9efb48396512b6a6b4b375
BigG <gabszon002@gmail.com>
2021-03-31 15:05:47 +0200

    Added npm bootstrap

--- CoffeeQuiz/src/app/app.module.ts
+++ CoffeeQuiz/src/app/app.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { PopupComponent } from './popup/popup.component';
 
 @NgModule({
-  declarations: [AppComponent, QuizComponent, LoadingSpinnerComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    QuizComponent,
+    LoadingSpinnerComponent,
+    PopupComponent,
+  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
