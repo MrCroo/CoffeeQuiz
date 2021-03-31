a173c4a084d0a42ed2bc3cf7b083d4c5e0ac61f7
BigG <gabszon002@gmail.com>
2021-03-31 12:02:23 +0200

    Added in score, loading, and if correct answer, it console.logs out

--- CoffeeQuiz/src/app/quiz/quiz.component.ts
+++ CoffeeQuiz/src/app/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuizDataService } from './../quiz-data.service';
 
 @Component({
   selector: 'quiz',
@@ -7,16 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
   answer: string = '';
-
+  data: any;
   inputText: string = '';
+  showSpinner: boolean = true;
+  score: number = 0;
 
-  constructor() {}
+  constructor(private quiz: QuizDataService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.quiz.getQuiz().subscribe((result) => {
+      this.data = result;
+      this.showSpinner = false;
+      console.log(this.data);
+    });
+  }
 
   addAnswer() {
     this.answer = this.inputText;
+    if (this.data[0].answer == this.answer) {
+      console.log('CORRECT ANSWER');
+      this.score += this.data[0].value;
+    } else {
+      console.log('Wrong Answer');
+    }
     this.inputText = '';
-    console.log(this.answer);
   }
 }
